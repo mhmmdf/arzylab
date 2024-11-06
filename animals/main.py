d1979d132c416e06f48250d00d3b95bf1f327c73
arzy <fikriarzyah2602@gmail.com>
2024-11-06 17:38:03 +0700

    menambahkan program sapaan animal

---
+++ animals/main.py
@@ -0,0 +1,4 @@
+input_animal = input("Masukkan jenis hewan peliharaan anda: ")
+input_name = input("Masukkan nama hewan peliharaan anda: ")
+
+print(f"Anda memiliki hewan peliharaan {input_animal} namanya {input_name}")
